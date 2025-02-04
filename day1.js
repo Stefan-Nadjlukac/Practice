c4a488dd39cedbc5c10fecf51daf2ba889c64425
Stefan-Nadjlukac <stefan.nadjlukac@automaticity.rs>
2025-02-04 14:57:52 +0100

    first assignment

---
+++ day1.js
@@ -0,0 +1,131 @@
+// 1.
+
+// let a = "Ovo je neki tekst";
+// let b = 5;
+// let c = false;
+
+// console.log(a, b, c);
+
+// 2.
+
+// let a = 5;
+// let b = 10;
+// let c = a + b;
+
+// console.log(c);
+
+// 3.
+
+// let a = 5;
+// let b = 10;
+// b += 3;
+// a = b;
+
+// console.log(a);
+
+// 4.
+
+// let a = 5;
+// let b = "5";
+
+// console.log(a);
+// console.log(b);
+// console.log(a == b);
+// console.log(a === b);
+
+// 5.
+
+// let a = 5;
+// let b = 14;
+// let c = a / b;
+
+// console.log(c);
+
+// 6.
+
+// let a = 6;
+// let b = 10;
+// let c = 12;
+// let d = a * b * c;
+
+// console.log(d);
+
+// 7.
+
+// let a = 2;
+// let b = 8;
+// let c = 10;
+
+// console.log((a + b + c) / 3);
+
+// 8.
+
+// let a = 14;
+// let b = 8;
+// let c = 20;
+// let avg = (a + b + c) / 3;
+
+// console.log(avg / 7);
+
+// 9.
+
+// let a = 10;
+// a += 7;
+
+// console.log(a);
+
+// 10.
+
+// let a = 10;
+// a *= 5;
+
+// console.log(a);
+
+// 11.
+
+// let a = 14;
+// let b = 8;
+
+// console.log(a - b);
+
+// 12.
+
+// console.log(
+//   "Tomorrow we’ll learn program flow control.",
+//   "\nEvery command must end with ;"
+// );
+
+// 13.
+
+// let a = 10;
+// let b = 8;
+
+// if (a * b > a + b) {
+//   console.log("Proizvod je veci od zbira");
+// } else {
+//   console.log("Proizvod je manji od zbira");
+// }
+
+// 14.
+
+// let startDate = new Date("2025-02-03T00:00:00");
+// let endDate = new Date("2025-02-03T20:31:16");
+
+// console.log((endDate - startDate) / 1000);
+
+// 15.
+
+// let a = 8;
+// let b = 4;
+// let obim = (a + b) * 2;
+// let povrsina = a * b;
+
+// console.log(
+//   `Obim pravougaonika: ` + obim + `\nPovrsina pravougaonika: ` + povrsina
+// );
+
+// 16.
+
+// let a = 7;
+
+// console.log(a * a);
